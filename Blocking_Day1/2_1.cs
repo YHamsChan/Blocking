8b7a7fd46844d92d4b596e222ea7615ad7bc6bd5
FictionFriend <78795430+YHamsChan@users.noreply.github.com>
2024-11-22 07:21:17 +0700

    Create 2_1.cs

---
+++ Blocking_Day1/2_1.cs
@@ -0,0 +1,13 @@
+using System
+
+namespace ConsoleApllication1 {
+  class Program {
+    static void Main() {
+      string Name;
+      Console.Write("\nMasukkan Nama Anda: ");
+      Name = Console.ReadLine();
+
+      Console.WriteLine("\nSelamat belajar C# {0}!", Name);
+    }
+  }
+}
